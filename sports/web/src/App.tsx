5f8d6e5ca5ce13eecbebcbdc73731380598dd421
Rodrigo Lopes <rodrigoczlopes@gmail.com>
2022-09-14 20:07:11 -0300

    mobile

--- sports/web/src/App.tsx
+++ sports/web/src/App.tsx
@@ -1,5 +1,25 @@
-function App() {
-  return <h1>Hello World!</h1>
+
+interface ButtonProps {
+  title: string;
 }
+
+function Button(props: ButtonProps){
+  return (
+    <button>
+      {props.title}
+    </button>
+  )
+}
+
+
+function App() {
+  return (
+  <div>
+    <Button title="Send 1" />
+    <Button title="Send 2"  />
+    <Button title="Send 3"  />
+  </div>
+  )}
+
 
 export default App
